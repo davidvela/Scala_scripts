037f55af5cafa78d03f63b9449049d371a44d586
davidvela <david.vela.tirado@gmail.com>
2020-05-17 14:20:14 +0200

    latest learning

--- datacamp_getstarted.scala
+++ datacamp_getstarted.scala
@@ -7,7 +7,7 @@ def DataCamp() // BlackJack
   // Print the difference
   println(difference ) 
 
-  // val = immutable; var = muttble 
+  // val = immutable; var = muttable 
   // Define immutable variables for clubs 2♣ through 4♣
   val twoClubs:   Int = 2
   val threeClubs: Int = 3
@@ -80,11 +80,67 @@ def ScalaBasis(){
 
 // Scala - Spark 
 def ScalaSparkBasis(){
-// 18 creating RDD
+
+// 18 RDD - (Resilient Distributed Database)
 val data = Array(1, 2, 3, 4, 5,6,7,8,9,10)
 val distData = sc.parallelize(data)
+distData.collect() /// see the content 
+val lines = sc.textFile("text.txt");
+lines.take(2) // see first 2 lines => Array(xx,yy)
+
+// 19 DataFrame - 
+// csv => packages com.databricks:spark-csv_2.10:1.3.0
+val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("train.csv")
+df.columns // colum names 
+df.count() // number of observations 
+df.printSchema() // print columns datatype 
+df.show(2)
+df.select("Age").show(10) // select columns 
+// filter rows 
+df.filter(df("Purchase") >= 10000).select("Purchase").show(10) 
+df.filter(df("Purchase") isin ("10000","2000")).select("Purchase").show(10)
+df.filter(df("Purchase") ===  10000).select("Purchase").show(10)
+df.groupBy("Age").count().show() 
+// apply SQL queries 
+df.registerTempTable("B_friday")  // register SQL table B_friday
+sqlContext.sql("select Age from B_friday").show(5)
+
+df.collect().foreach(row => <do something>)
 
 
+
+// 20 ML model 
+val df1 = df.select("User_ID","Occupation","Marital_Status","Purchase")
+// -- import formula
+import org.apache.spark.ml.feature.RFormula
+val formula = new RFormula().setFormula("Purchase ~ User_ID+Occupation+Marital_Status").setFeaturesCol("features").setLabelCol("label")
+val train = formula.fit(df1).transform(df1)
+// -- linear regression 
+import org.apache.spark.ml.regression.LinearRegression
+val lr = new LinearRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+val lrModel = lr.fit(train)
+println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+val trainingSummary = lrModel.summary
+Now, See the residuals for train's first 10 rows.
+trainingSummary.residuals.show(10)
+println(s"RMSE: ${trainingSummary.rootMeanSquaredError}") 
+val train = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("train.csv")
+val splits = train.randomSplit(Array(0.7, 0.3))
+val (train_cv,test_cv) = (splits(0), splits(1))
+import org.apache.spark.ml.feature.RFormula
+val formula = new RFormula().setFormula("Purchase ~ User_ID+Occupation+Marital_Status").setFeaturesCol("features").setLabelCol("label")
+val train_cv1 = formula.fit(train_cv).transform(train_cv)
+val test_cv1 = formula.fit(train_cv).transform(test_cv)
+import org.apache.spark.ml.regression.LinearRegression
+val lr = new LinearRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+val lrModel = lr.fit(train_cv1)
+val train_cv_pred = lrModel.transform(train_cv1)
+val test_cv_pred = lrModel.transform(test_cv1)
+
+
+// end 
 
 }
 
+
+// scala partitions: https://medium.com/@mrpowers/managing-spark-partitions-with-coalesce-and-repartition-4050c57ad5c4
